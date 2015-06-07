dcf964a4f64a4ac4a847264ea5dbe36c7ed68a48
Brendan <brendanbath@gmail.com>
2015-06-07 18:36:15 +0200

    Started up
    created login screen ect

---
+++ src/main/java/com/Brendan/other/Main.java
@@ -0,0 +1,38 @@
+package main.java.com.Brendan.other;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static Employee current = new Employee();
+    public static String ID;
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+
+        String title= "TTMS";
+        primaryStage.setTitle(title);
+        Stage popUp = new Stage();
+        popUp.setTitle(title);
+        popUp.centerOnScreen();
+        popUp.setResizable(false);
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent popup = fxmlLoader.load(getClass().getResource("/fxml/loginPopup.fxml").openStream());
+        popUp.setScene(new Scene(popup));
+
+        popUp.show();
+
+    }
+
+}
